dc4a30fce7cfcd88dedb173e2bf89d77ee98ab8d
jzhang6 <jenny.zhang@morningstar.com>
2022-07-24 21:01:45 +0800

    delete redundant import

--- src/test/java/util/ScreenShot.java
+++ src/test/java/util/ScreenShot.java
@@ -3,7 +3,6 @@ package util;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
